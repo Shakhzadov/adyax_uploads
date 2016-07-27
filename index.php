9f80fd6b264c5051876d7188ff9a0883911bf486
Shakhzadov <k.shakhzadov@gmail.com>
2016-07-27 23:01:36 +0300

    File upload work, list of uploaded files is shown

---
+++ index.php
@@ -0,0 +1,36 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<title>Завантаження файлів</title>
+	<style>
+		form {
+			width: 400px;
+			margin: 50px auto;
+			text-align: center;
+		}
+	</style>
+</head>
+<body>
+	<form action="upload.php" method="POST" enctype="multipart/form-data">
+		<h2>Виберіть файли для завантаження</h2>
+		<input type="file" name="filename"/>
+		<input type="submit" value="Завантажити"/>		
+	</form>
+	
+	<h2>Files</h2>
+
+	<?php
+		$handle = opendir('uploads/');
+		if($handle) {
+			while(($entry = readdir($handle)) !== false) {
+				if($entry != '.' && $entry != '..') {
+					echo "<a href='uploads/$entry'>$entry</a><br/>";
+				}
+			}
+			closedir($handle);
+		}
+	?>
+
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,28 @@
+﻿<?php
+
+if(isset($_FILES['filename'])) {	
+	$file = $_FILES['filename'];
+	$name = $file['name'];
+	$type = $file['type'];
+	$tmp_location = $file['tmp_name'];
+	$final_destination = 'uploads/'.$name;
+	$error = $file['error'];
+	$max_upload_size = 2097152;
+	$size = $file['size'];
+
+	if($error > 0 || is_array($error)) {
+		die('Sorry an error occured');
+	}
+
+	if(file_exists($final_destination)) {
+		die('Sorry that file already exist');
+	}
+
+	if(!move_uploaded_file($tmp_location, $final_destination)) {
+		die('Cannot finish upload, something went wrong');
+	}
+}
+
+?>
+
+<h2>File Successfully Uploaded!</h2>
